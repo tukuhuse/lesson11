70f693ece760ccf27626a1f7c77974d25b9a32da
EC2 Default User <ec2-user@ip-172-31-29-213.us-east-2.compute.internal>
2020-01-26 16:00:15 +0000

    practiceは完成版、kadai05は原本ここから修正を加える

---
+++ kadai05.php
@@ -0,0 +1,17 @@
+$2018_calender = {
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => 10月,
+  "November" => "11月",
+  "December" => "12月"
+};
+
+// 12月を表示する
+echo $2018_calender[December];
---
+++ practice.php
@@ -0,0 +1,13 @@
+<?php
+$a=3;
+$b=7;
+echo $a+$b . "\n";
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7] . "\n";
+$hello="Hello,";
+$name="Yuto";
+$world="'s World!";
+echo $hello . $name . $world . "\n";
+$tech_boost="tech ";
+$tech_boost.="boost";
+echo $tech_boost . "\n";
